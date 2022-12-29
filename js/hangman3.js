bf86d370c44fc1fa513926d2a9d6e7cc3224d4aa
lutxi <aliaga_sheila@hotmail.com>
2022-12-29 08:18:52 +0100

    uppdating

--- js/hangman3.js
+++ js/hangman3.js
@@ -1,29 +1,166 @@
 $(function() {
+
+    $(".reset-game").hide();
    
-const WORDSARRAY = ["cheer", "table", "beach", "green", "drive", "hello", "admit", "earth", "image", "brown", "mouse", "house", "water", "drink"];
-const WORD = WORDSARRAY[ (Math.floor(Math.random() * WORDSARRAY.length))];
+    const WORDSARRAY = ["cheer", "table", "beach", "green", "drive", "hello", "admit", "earth", "image", "brown", "mouse", "house", "water", "drink"];
+    
+    let newWord = WORDSARRAY[ (Math.floor(Math.random() * WORDSARRAY.length))];
+    console.log(newWord);
+    const LETTERSARRAY = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    
+    let wins = 0;// count the total number of wins
+    let lose = 0;// counmt the total number of losses
 
-const LETTERSARRAY = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    let hiddenWord = "-----";
+    let letter;
+    let lettersEntered = [];    // new array for the letters entered, will add one by one in the game
+    
+    let attempts = 12; // number of attempts
+
+    let letterArray = [];
+
+    for (let b = 1; b <= 12; b++) {                             //All the pictures hiden from the begining
+        $(".picture").append('<img src="./images/'+b+'.jpg" alt="hangman" hidden=true id="' + b +'" />');
+
+    }
+   
+    $(".guessword").text(hiddenWord);
+    $(".attempts").text("Atempts: " + attempts + " of 12");
+    $(".letters_entered").text("Letters already entered: " + lettersEntered);
 
 
-$(".picture").append('<img src="./images/12.jpg" alt="hangman" />');
+    for (let k = 0; k < LETTERSARRAY.length; k++) {   //to get the keyboard from the LETTERSARRAY.
+        $("<button class='buttons'>"+LETTERSARRAY[k]+"</button>").appendTo(".keyboard"); 
+    }
+
+    $(".buttons").on( {
+        mouseenter: function() {
+            $(this).addClass("activebutton");
+        },
+        mouseleave: function(){
+            $(this).removeClass("activebutton");
+        },
+        click: function() {
+            //
+            letter= $(this).text().toLowerCase();
+
+            if(attempts > 0){
+                if(newWord.indexOf(letter) != -1) {     // check if the letter is in the word
+                //the user enter the right code
+                    lettersEntered.push(letter);  // add letter into the array
+                    letterArray = findIndexes(letter, newWord);   // calling function that will return array of indexes
+                   
+                    for(let j = 0; j < letterArray.length; j++){     //replace the "-" with the entered letter
+                        hiddenWord = replaceAt(hiddenWord,letterArray[j],letter);
+                    }
+                    if(hiddenWord === newWord){    // final conditional if the whole word is guessed or not.
+                        $(".section2").hide();
+                        $(".keyboard").hide();
+                        $(".section3").text("CONGRATULATIONS!!! YOU WIN.The word was: " + newWord).show();
+                        $(".reset-game").show();
+                        wins++;
+                    }else{
+                        $(".guessword").text(hiddenWord);
+                        $(".attempts").text("Atempts: " + attempts + " of 12");
+                        $(".letters_entered").text("Letters already entered: " + lettersEntered);
+    
+                    }
 
 
 
-for (let k = 0; k < LETTERSARRAY.length; k++) {   //to get the keyboard from the lettersArray.
-    $("<button class='buttons'>"+LETTERSARRAY[k]+"</button>").appendTo(".keyboard"); 
+                }else{                  //the user enter the wrong letter
+                    if(attempts === 12) {
+                        $("#" + attempts).show();
+                        attempts--;
+                    }else{
+                        $("#"+ (attempts + 1)).hide();
+                        $("#" + attempts).show();
+                        attempts--;
+
+                        if(attempts === 0) {
+                            $(".section2").hide();
+                            $(".keyboard").hide();
+                            $(".section3").text("GAME OVER, click reset to start again").show();
+                            $("reset-game").show();
+                            lose++;
+                        }
+                    }
+                    
+                    lettersEntered.push(letter);  // add letter into the array
+
+                    $(".guessword").text(hiddenWord);
+                    $(".attempts").text("Atempts: " + attempts + " of 12");
+                    $(".letters_entered").text("Letters already entered: " + lettersEntered);
+
+                }
+
+            }else{
+               $(".section2").hide();
+               $(".keyboard").hide();
+               $(".section3").text("GAME OVER, click reset to start again");
+               $("reset-game").show();
+               //lose++;
+            }
+        }
+    })
+
+
+
+
+function findIndexes(l, w){  // l for letter and w for word, two variables that i do not want to mix with the above already write.
+
+    let arrayOfIndexes = [];
+
+    for(let i = 0; i < w.length; i++) {
+        if(l === w[i]){                     // if the letter is in the word will add its index in the array of indexes
+            arrayOfIndexes.push(i);
+        }
+    }
+    return arrayOfIndexes;
 }
 
-$(".buttons").on( {
+function replaceAt(hWord,index,letter){
+    if(index === 0) {
+        return  letter + hWord.substring(1, hWord.length);   // this is for the first letter to replace
+    }else if (index === hWord.length-1) {                                    // this is for the last letter to replace
+        return hWord.substring(0, hWord.length-1) + letter;
+    }else {
+        return hWord.substring(0, index) + letter+hWord.substring(index+1,hWord.length);   //this is for the middle letters to replace
+    } 
+}
+
+$("<button class='reset'>RESET</button>").appendTo(".reset-game");
+
+$(".reset").on( {
     mouseenter: function() {
         $(this).addClass("activebutton");
     },
     mouseleave: function(){
         $(this).removeClass("activebutton");
     },
-  //  click: function() {
-  //      $(this).
-    })
+    click: function() {
+        $(".section3").hide();
 
+        hiddenWord="-----";
+        if(attempts < 12){
+            $("#" + (attempts+1)).hide();
+        }
+        attempts = 12; 
+        lettersEntered = [];   
+        newWord = WORDSARRAY[ (Math.floor(Math.random() * WORDSARRAY.length))];
+        console.log(newWord);
+
+        $(".guessword").text(hiddenWord);
+        $(".attempts").text("Atempts: " + attempts + " of 12");
+        $(".letters_entered").text("Letters already entered: " + lettersEntered);
+
+        $(".section2").show();
+        $(".keyboard").show();
+        $(".reset-game").hide();
+
+    }
+    
 })
 
+
+})
